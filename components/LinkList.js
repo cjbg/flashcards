c682ea3b63a98aec23e35996570c7d7d0acf5b6b
cjbg <jczabator@gmail.com>
2016-11-27 23:23:59 +0100

    start refactor

---
+++ components/LinkList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import{
+    TouchableHighlight,
+} from 'react-native';
+
+import { TaskRow } from '../components/TaskRow';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 15,
+    }
+});
+
+{/* TODO: Unify one StyleSheet
+    Refactor for LinkRow
+     */}
